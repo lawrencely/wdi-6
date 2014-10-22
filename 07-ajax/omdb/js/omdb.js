c4d828a23bb4b5c5fc198c170d2da2b931242d49
lawrencely <lawrence.ly667@gmail.com>
2014-10-22 12:38:14 +1100

    cleaning up directories

--- 07-ajax/omdb/js/omdb.js
+++ 07-ajax/omdb/js/omdb.js
@@ -1,5 +1,37 @@
 $(document).ready(function () {
-  $.ajax('http://www.omdbapi.com/?t=True%20Grit&y=1969').done(function (result) {
-    console.log(result);
-  });
+   $('#search').on('click', findMovie);
+   $('#multipleSearch').on('click', multipleMovie);
+   $('#clearHistory').on('click', clearHistory);
 });
+
+var findMovie = function () {
+
+  var movieSearch = $('#movieSearch').val();
+  var url = 'http://www.omdbapi.com/?t=' + movieSearch;
+  $.ajax( url , {dataType: 'json'}).done(function (result) {
+    $movie = $('<div>', {'class': 'title'});
+    $movie.append($('<p>').text('Title: ' + result.Title));
+    $movie.append($('<p>').text('Year: ' + result.Year));
+    $movie.append($('<p>').text('Plot: ' + result.Plot));
+    $('<img>', { src: result.Poster }).appendTo($movie);
+    $movie.appendTo('.container');
+  });
+}
+
+var multipleMovie = function () {
+
+  var movieSearch = $('#movieSearch').val();
+    $.ajax('http://www.omdbapi.com/?s=' + movieSearch, {dataType: 'json'}).done(function (result) {
+      $.each(result.Search, function ( index ,value) {
+      $movies = $('<div>', {'class': 'multipleMovie'});
+      $movies.append($('<p>').text('Title: ' + value.Title ));
+      $movies.append($('<p>').text('Year:  ' + value.Year  ));
+      $movies.appendTo('.multipleContainer');
+    });
+  });
+}
+
+var clearHistory = function () {
+  ('#movieSearch').empty();
+  ('.container').empty();
+}

d7d92e9f6a818a4b5c7e7552c42d34c31ff75aeb
lawrencely <lawrence.ly667@gmail.com>
2014-11-13 15:01:20 +1100

    100% test coverage

---
+++ 10-advanced/fruitstore_app/app/models/fruit.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: fruits
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  name       :string(255)
+#  type       :string(255)
+#
+
+class Fruit < ActiveRecord::Base
+  belongs_to :shelf
+end
---
+++ 10-advanced/fruitstore_app/config/routes.rb
@@ -0,0 +1,12 @@
+# == Route Map
+#
+# You don't have any routes defined!
+#
+# Please add some routes in config/routes.rb.
+#
+# For more information about routes, see the Rails guide: http://guides.rubyonrails.org/routing.html.
+#
+
+Rails.application.routes.draw do
+  resources :fruits, :only => [:index]
+end

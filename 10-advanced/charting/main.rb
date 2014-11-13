31351af2aed12b85cb346392eece8ffc06c7a3d7
lawrencely <lawrence.ly667@gmail.com>
2014-11-14 10:12:21 +1100

    added json

--- 10-advanced/charting/main.rb
+++ 10-advanced/charting/main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/' do
   erb :index
@@ -7,4 +8,17 @@ end
 
 get '/morris' do
   erb :morris
+end
+
+get '/beverages.json' do
+  content_type :json
+  [
+    {day: 'Friday', coffee: 3, water: 1, beer: 9, whiskey: 9},
+    {day: 'Saturday', coffee: 2, water: 1, beer: 10, whiskey: 0},
+    {day: 'Sunday', coffee: 1, water: 1, beer: 0, whiskey: 10},
+    {day: 'Monday', coffee: 5, water: 1, beer: 2, whiskey: 2},
+    {day: 'Tuesday', coffee: 4, water: 1, beer: 4, whiskey: 6},
+    {day: 'Wednesday', coffee: 3, water: 1, beer: 3, whiskey: 3},
+    {day: 'Thursday', coffee: 4, water: 1, beer: 2, whiskey: 5}
+  ].to_json
 end

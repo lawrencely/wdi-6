83655be9c1222801917834dfeaa7b1315399389e
lawrencely <lawrence.ly667@gmail.com>
2015-01-29 23:25:35 +1100

    finally finished. kinda haha

--- pre-interview_test/lib/page.rb
+++ pre-interview_test/lib/page.rb
@@ -1,13 +1,52 @@
-Page = Struct.new(:name, :content) do
+require 'json'
+require 'pry'
+
+
+Page = Struct.new(:name, :content, :links) do
 	def self.from_map(map)
 		# implement from_map to pass the specifications found in page_spec.rb
-    Page.new({name: map['name'], content: map['content']})
+    page = Page.new(map['name'], map['content'], map['links'])
+    return page
 	end
-end
 
-class Page
-  def initializer(name, content)
-    @name = name
-    @content = content
+  def self.ingest(json)
+    list =  JSON.parse(json)
+    #parse json to an array of hashes
+    list.map do |row|
+      Page.new(row['name'], row['content'])
+    end
+
+  end
+
+  def self.ingest2(json)
+    list =  JSON.parse(json)
+
+    # Creates Pages from json collection
+    pages = list.map do |row|
+      Page.new(row['name'], row['content'], row['links'])
+    end
+
+    # Replace text links with the matched Pages
+    pages.each do |page|
+      if page.links != nil
+        # find related pages
+        link_pages = find_pages_by_name(page.links, pages)
+        # replace strings with Pages
+        page.links = link_pages.flatten
+      end
+    end
+  end
+
+  # returns an array of pages
+  def self.find_pages_by_name(links, pages)
+    link_pages = []
+    links.each do |link|
+      matches = pages.select{ |page| page.name == link }
+      link_pages << matches if !matches.empty?
+    end
+    link_pages
   end
 end
+
+# currently doesnt alert users if links refers to unknown Page.name
+# also doesnt alert users that Page.name already exist
--- pre-interview_test/spec/page_spec.rb
+++ pre-interview_test/spec/page_spec.rb
@@ -3,43 +3,64 @@ require 'page'
 describe Page do
   describe '.new' do
       subject(:page) { Page.new('name', 'content') }
-      
-      it 'responds with the page name' do 
+
+      it 'responds with the page name' do
         expect(page.name).to eq('name')
       end
-      
-      it 'responds with the page content' do 
+
+      it 'responds with the page content' do
         expect(page.content).to eq('content')
       end
   end
-  
+
   describe '.from_map' do
     it 'converts a map into a page' do
       map = {'name' => 'Name', 'content' => 'The content.'}
       expect(Page.from_map(map)).to eq(Page.new('Name', 'The content.'))
     end
-    
+
     it 'handles maps without all page attributes' do
       map = {'name' => 'Contentless'}
       expect(Page.from_map(map)).to eq(Page.new('Contentless'))
     end
   end
-  
-  describe '.ingest' do    
+
+  describe '.ingest' do
     it 'converts a json list of objects into instances of Page' do
-    	pages = Page.ingest <<-JSON 
-        [
-          {"name": "one", "content": "one content"},
-          {"name": "two", "content": "two content"}
-        ]
+    	pages = Page.ingest <<-JSON
+        [{"name": "one", "content": "one content"},{"name": "two", "content": "two content"}]
       JSON
-    	
+
     	expect(pages).to eq(
-    		[ 
+    		[
     			Page.new('one', 'one content'),
     			Page.new('two', 'two content')
         ]
       )
     end
   end
-end
+
+  describe '.ingest2' do
+    it 'should return an array' do
+      pages = Page.ingest2 <<-JSON
+        [
+          {
+            "name": "parent",
+            "links": ["child1", "child2"]
+          },
+          { "name": "child1" },
+          { "name": "child2" }
+        ]
+      JSON
+
+      child1 = Page.from_map('name' => 'child1')
+      child2 = Page.from_map('name' => 'child2')
+      parent = Page.from_map('name' => 'parent', 'links' => [child1, child2])
+      expect(pages).to eq(
+        [
+          parent, child1,child2
+        ]
+      )
+    end
+  end
+ end
